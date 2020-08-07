fb568c3b82d03dab5c291ee18a2bfd762a6f0136
Gupta-boss <guptaveer00@gmail.com>
2020-08-07 11:05:49 -0400

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # angryBirdsStage2
 Angry Birds stage 2 with Class Inheritance and Images
+added platform 
+#### to be completed 
+- the slingshot 
+- smoke image along bird path 
--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-    platformOBJ = new Ground(100, height-100, 200, 200);
+    platformOBJ = new Ground(101, height-100, 200, 200);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
